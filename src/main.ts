737b9e4b545817d21d94b58fec0ebb5f8d779b7e
Ilya Orlov <orloveagle@mail.ru>
2023-04-01 01:11:33 +0300

    chore: eslint rules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const config = await app.get(ConfigService)
+  const port = config.get('API_PORT')
+
+  await app.listen(port, () => {
+    console.log(`STH server is listening on ${port} port...`)
+  })
 }
 
-bootstrap();
+bootstrap()
